7f24e1669d9882bd6d468ae8f6fedf623ed424ea
Unknown <jeanlouis.carron@student.kuleuven.be>
2019-04-01 11:31:38 +0200

    dsqmflkj

---
+++ OGP Practicum 1/src/filesystem/Directory.java
@@ -0,0 +1,22 @@
+package filesystem;
+
+import java.util.ArrayList;
+import be.kuleuven.cs.som.annotate.*;
+
+/**
+ * A class of directories.
+ * 
+ * 
+ * 
+ * @author Robin Bruneel, Jean-Louis Carron, Edward Wiels
+ * @version 1.0
+ *
+ */
+
+public class Directory extends Item {
+
+	public Directory(String name, boolean writable) {
+		
+	}
+	
+}
