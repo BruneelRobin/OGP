b700858f124b7057d8a71d600fc60f4b2dd73c9d
JLow <jeanlouis.carron@student.kuleuven.be>
2019-05-08 17:40:10 +0200

    update

--- Project/src/MindCraft/Weapon.java
+++ Project/src/MindCraft/Weapon.java
@@ -6,8 +6,41 @@ package MindCraft;
  * @author 	Robin Bruneel, Jean-Louis Carron, Edward Wiels
  * @version 1.0 - 2019
  */
-public class Weapon {
+public class Weapon extends Item {
+	
+	
 
+	/**
+	 * Class variable referencing the maximum damage of any weapon.
+	 */
+	private static int MAX_DAMAGE = 100;
 	
+	/**
+	 * Class variable referencing the minimum damage of any weapon.
+	 */
+	private static int MIN_DAMAGE = 1;
 	
+	/**
+	 * Variable referencing the damage of a weapon.
+	 */
+	private int damage = 0;
+	
+	/**
+	 * Set the damage of a weapon to a given amount of damage.
+	 * 
+	 * @param	damage
+	 * 			The new damage.
+	 * @post	The damage is set to the given damage.
+	 */
+	private void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	/**
+	 * Return the damage of this weapon.
+	 * @return	Return the damage of this weapon.
+	 */
+	public int getDamage() {
+		return this.damage;
+	}
 }
