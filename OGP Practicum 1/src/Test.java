32642fc75602e41cfc9d4293782e03ceb20a75b5
JLow <jeanlouis.carron@student.kuleuven.be>
2019-05-15 18:17:52 +0200

    Testen is cruciaal, het zorgt ervoor dat we onze fouten ontdekken en vervolgens oplossen. Wow

--- OGP Practicum 1/src/Test.java
+++ OGP Practicum 1/src/Test.java
@@ -8,8 +8,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Monster Bob = new Monster("Bob",4,4,4);
-		System.out.println(Bob.isValidName("Bob"));
 		
 	}
  
---
+++ Project/src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+import MindCraft.*;
+import MindCraft.Character;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Monster Bob = new Monster("679",4,4,4);
+	}
+
+}
